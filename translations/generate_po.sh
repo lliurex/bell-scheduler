72587627f13a638de51e15b9719caa16d5eccff4
jrpelegrina <juapesai@hotmail.com>
2018-11-26 08:48:04 +0100

    Clean translations folder

--- translations/generate_po.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-xgettext --join-existing ./bell-scheduler/python3-bellscheduler/MainWindow.py -o ./translations/bell-scheduler.pot
-xgettext --join-existing ./bell-scheduler/python3-bellscheduler/BellBox.py -o ./translations/bell-scheduler.pot
-xgettext --join-existing ./bell-scheduler/python3-bellscheduler/rsrc/bell-scheduler.ui -o ./translations/bell-scheduler.pot
